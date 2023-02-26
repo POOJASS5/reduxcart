f185f64223c16809a0bd6bdbe5b0ce35368ac4ed
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-26 18:52:32 +0530

    adding cart item count to cart button

--- CartButton.js
+++ CartButton.js
@@ -1,21 +1,20 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import classes from "./CartButton.module.css";
 import { cartActions } from "../reduxStore/cartSlice";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const quantity = useSelector((state) => state.cart.totalQuantity);
 
   const showCartHandler = () => {
     dispatch(cartActions.showCart());
   };
-
   return (
     <button className={classes.button} onClick={showCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{quantity}</span>
     </button>
   );
 };
-
 export default CartButton;
